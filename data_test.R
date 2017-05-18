12955bf543283ae901b494a1870848aeae12fa87
jsiegelman1 <jacob.siegelman1@gmail.com>
2017-05-17 19:01:29 -0700

    Added department naming from abbreviations, we may want to do this before hand and just make a CSV. Close #2

--- data_test.R
+++ data_test.R
@@ -5,5 +5,14 @@ library(plotly)
 grade_data <- read.csv("grade_data.csv", stringsAsFactors = FALSE)
 grade_data <- grade_data %>% mutate(department = str_match(Course_Number, '[^0-9]*'))
 
+
+majors <- "http://www.washington.edu/students/reg/depttools/tsupdate/major.php"
+major.list <- readHTMLTable(majors, header=TRUE, stringsAsFactor = FALSE)
+major.table <-bind_rows(major.list)
+colnames(major.table) <- c("abbr", "name")
+major.table <- major.table %>% mutate(name = trimws(str_match(name, '[^();-]*'))) %>%
+  distinct(abbr, .keep_all = TRUE)
+
+
 new_data <- grade_data %>% group_by(department, Term) %>% summarize(classes = n(), class_size = mean(Student_Count), mean_gpa = mean(Average_GPA))
 map <- plot_ly(new_data, x = new_data$class_size, y = new_data$mean_gpa, color = "blue")
