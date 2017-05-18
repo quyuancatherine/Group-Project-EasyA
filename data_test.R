b370db3eb1d090b26097f031daf96eac387c43f2
jsiegelman1 <jacob.siegelman1@gmail.com>
2017-05-17 18:00:11 -0700

    fixed overwriting data

--- data_test.R
+++ data_test.R
@@ -5,5 +5,5 @@ library(plotly)
 grade_data <- read.csv("grade_data.csv", stringsAsFactors = FALSE)
 grade_data <- grade_data %>% mutate(department = str_match(Course_Number, '[^0-9]*'))
 
-grade_data <- grade_data %>% group_by(department, Term) %>% summarize(classes = n(), class_size = mean(Student_Count), mean_gpa = mean(Average_GPA))
-map <- plot_ly(grade_data, x = grade_data$class_size, y = grade_data$mean_gpa, color = "blue")
+new_data <- grade_data %>% group_by(department, Term) %>% summarize(classes = n(), class_size = mean(Student_Count), mean_gpa = mean(Average_GPA))
+map <- plot_ly(new_data, x = new_data$class_size, y = new_data$mean_gpa, color = "blue")
