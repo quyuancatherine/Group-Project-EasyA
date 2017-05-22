e430f5d0fb1d1e3247c83ce552f8c23ce25c4c1a
jsiegelman1 <jacob.siegelman1@gmail.com>
2017-05-22 15:12:33 -0700

    Finished with department name scraping. There are a lot of holes, lets talk about it.

--- data_test.R
+++ data_test.R
@@ -2,17 +2,18 @@ setwd("~/Documents/School/info201/Group-Project-EasyA")
 library(stringr)
 library(dplyr)
 library(plotly)
+library(XML)
 grade_data <- read.csv("grade_data.csv", stringsAsFactors = FALSE)
-grade_data <- grade_data %>% mutate(department = str_match(Course_Number, '[^0-9]*'))
-
 
 majors <- "http://www.washington.edu/students/reg/depttools/tsupdate/major.php"
 major.list <- readHTMLTable(majors, header=TRUE, stringsAsFactor = FALSE)
 major.table <-bind_rows(major.list)
-colnames(major.table) <- c("abbr", "name")
-major.table <- major.table %>% mutate(name = trimws(str_match(name, '[^();-]*'))) %>%
-  distinct(abbr, .keep_all = TRUE)
+colnames(major.table) <- c("department", "department_name")
+major.table <- major.table %>% mutate(department_name = trimws(str_match(department_name, '[^();]*'))) %>%
+  distinct(department, .keep_all = TRUE)
+
+grade_data <- grade_data %>% mutate(department = trimws(str_match(Course_Number, '[^0-9]*'))[,1]) %>% left_join(major.table)
 
 
-new_data <- grade_data %>% group_by(department, Term) %>% summarize(classes = n(), class_size = mean(Student_Count), mean_gpa = mean(Average_GPA))
+new_data <- grade_data %>% group_by(department, department_name, Term) %>% summarize(classes = n(), class_size = mean(Student_Count), mean_gpa = mean(Average_GPA))
 map <- plot_ly(new_data, x = new_data$class_size, y = new_data$mean_gpa, color = "blue")
