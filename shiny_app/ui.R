1581f33c2dc7e95f4c5089acd1877d4d249e77c0
jsiegelman1 <jacob.siegelman1@gmail.com>
2017-05-22 14:38:13 -0700

    Added faq file and incorporated it into the shiny app.

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -10,24 +10,14 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
+shinyUI(
+  navbarPage("My Application",
+             tabPanel("Component 1",
+                      mainPanel("First Page")
+             ),
+             tabPanel("Component 2",
+                      mainPanel("Second Page")),
+             tabPanel("FAQ",
+                      mainPanel(shiny::includeHTML("./static/faq.html")))
   )
-))
+)
