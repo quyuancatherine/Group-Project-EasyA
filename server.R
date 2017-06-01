d06df82df3ba9b4ca622ced817ccecba4ccb6799
jsiegelman1 <jacob.siegelman1@gmail.com>
2017-06-01 04:59:46 -0700

    Finished the project

--- server.R
+++ server.R
@@ -1,19 +1,69 @@
+
 library(shiny)
-
-data <- read.csv(file = 'dgrade_data.tsv', header = TRUE)
-
-# Define server logic required to draw a histogram
+library(plotly)
+library(XML)
+library(stringr)
+library(dplyr)
+library(plotly)
+library(XML)
+library(jsonlite)
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+  # Load Data
+  grade.data <- read.csv("full_data.csv", stringsAsFactors = FALSE)
+  # Department Table
+  by_department <- grade.data %>% group_by(department) %>%
+    summarize(classes = n(), students = sum(Student_Count), mean_students = students/classes, mean_gpa = mean(Average_GPA)) %>% arrange(department)
+  colnames(by_department) <- c("Department Code", "Total Classes", "Total Students", "Mean Enrollment", "Mean GPA")
+  output$department_table <- renderDataTable(by_department, options = list(pageLength = 10, autowidth = TRUE))
+  
+  # Other Charts
+  source('./scripts/script.R')
+  output$histogram <- renderPlotly({
+    return(HistChart(grade.data, input$`course number`, input$instructor, input$term))
+  })
+  
+  source('./scripts/plot.R')
+  output$chart <- renderPlotly({
+    return(PlotChart(grade.data, input$department, input$time))
+  })
+  
+  
+  
+  a <- reactive({
+    x <- filter(grade.data, Course_Title == input$course.var)
+    y <- unique(x$department_name)
+    return(y)
+  })
+  
+  output$depControl <- renderUI({
+    selectInput('dep.var', 'Select Department', choices = a())
+  })
+  
+  b <- reactive({
+    x <- filter(grade.data, Course_Title == input$course.var, department_name == input$dep.var)
+    return(x)
+  })
+  
+  output$plotly <- renderPlotly({
+    plot_ly(type = 'scatter', data = b(), x = ~Academic_Year, y = ~Average_GPA, sizes = ~Average_GPA, color = ~Course_Number, text = ~paste('Section: ', Course_Number)) %>% layout(margin = list(b = 160))
+  })
+  
+  output$prof_table <- reactive({
+    by_professor <- grade.data %>% filter(Primary_Instructor == input$prof.var) %>% group_by(department_name, Course_Title) %>% summarize(terms = n(), avg.grade = mean(Average_GPA), mean_students = mean(Student_Count))
+    renderDataTable(by_professor, options = list(pageLength = 10, autowidth = TRUE))
+  })
+  output$prof_graph <- renderPlotly({
+    by_professor <- grade.data %>% filter(Primary_Instructor == input$prof.var) %>% group_by(department_name, Course_Title) %>% summarize(avg.grade = mean(Average_GPA))
+    plot_ly(by_professor, x = ~Course_Title, y = ~avg.grade, type = 'bar') %>% layout(margin = list(b = 200), xaxis = list(title = "Course Title"), yaxis = list(title = "Average GPA"))
+  })
+  output$extremes <- renderPlotly({
+    cur_department <- grade.data %>% filter(department_name == input$dept.var) %>% group_by(Primary_Instructor) %>% summarize(classes = n(), avg.grade = mean(Average_GPA))
+    top <- cur_department %>% top_n(5)
+    bottom <- cur_department %>% top_n(-5)
+    avg <- grade.data %>% filter(department_name == input$dept.var) %>% summarize(classes = n(), avg.grade = mean(Average_GPA)) %>% mutate(Primary_Instructor = "Average")
+    avg[1, 'classes'] = 0
+    tot <- bind_rows(top, avg, bottom) %>% arrange(-avg.grade)
+    plot_ly(tot, x = ~Primary_Instructor, y = ~avg.grade, type = 'bar') %>% layout(margin = list(b = 160), xaxis = list(title = "Course Title", categoryorder = "array", categoryarray = tot$Primary_Instructor), yaxis = list(title = "Average GPA/Number of Classes taught"))
   })
   
 })
---
+++ ui.R
@@ -0,0 +1,116 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(plotly)
+
+grade.data <- read.csv("full_data.csv")
+course.num <- unique(grade.data$Course_Number)
+instructor.name <- unique(grade.data$Primary_Instructor)
+term <- c("Autumn 2010", 
+          "Autumn 2011", "Winter 2011", "Spring 2011", "Summer 2011", 
+          "Autumn 2012", "Winter 2012", "Spring 2012", "Summer 2012",
+          "Autumn 2013", "Winter 2013", "Spring 2013", "Summer 2013",
+          "Autumn 2014", "Winter 2014", "Spring 2014", "Summer 2014",
+          "Autumn 2015", "Winter 2015", "Spring 2015", "Summer 2015",
+          "Winter 2016")
+unique.course <- unique(grade.data$Course_Title)
+unique.prof <- unique(grade.data$Primary_Instructor)
+unique.dept <- unique(grade.data$department_name)
+
+
+
+# Define UI for application that draws a histogram
+shinyUI(
+  navbarPage("University of Washington Grade Data",
+             tabPanel("About",
+                      mainPanel(shiny::includeHTML("./static/faq.html"))),
+             tabPanel("Class Overview",
+                      fluidPage(
+                        titlePanel("Search by Class"),
+                        sidebarLayout(
+                          sidebarPanel(
+                            
+                            selectInput("course number", 
+                                        label = "Course Number",
+                                        choices = course.num,
+                                        selected = "A A 198 A"),
+                            
+                            selectInput("instructor", 
+                                        label = "Instructor",
+                                        choices = instructor.name,
+                                        selected = 'KNOWLEN, CARL'),
+                            
+                            selectInput("term", 
+                                        label = "Term",
+                                        choices = term,
+                                        selected = 'Autumn 2010')
+                          ),
+                          
+                          mainPanel(plotlyOutput("histogram"))
+                        )
+                      )),
+             tabPanel("Department Table",
+                      mainPanel(dataTableOutput("department_table"))
+             ),
+             tabPanel("Department Overview",
+                      fluidPage(
+                        titlePanel("Department GPA"),
+                        
+                        sidebarLayout(
+                          sidebarPanel(
+                            helpText("Create a bar plot by choosing your department of interest"),
+                            
+                            selectInput("department", 
+                                        label = "choose a department to display",
+                                        choices = unique.dept,
+                                        selected = "Aeronautics & Astronautics"),
+                            
+                            helpText(p('By choosing the time range, the plot will only display the department you choose
+                                       within that time range.')),
+                            
+                            selectInput("time", 
+                                        label = "choose a time range to display",
+                                        choices = c('2010', '2011', '2012', '2013', '2014', '2015'),
+                                        selected = '2010')
+                            ),
+                          
+                          mainPanel(plotlyOutput("chart"))
+                        )
+             )),
+             tabPanel("Course Search", fluidPage(
+               
+               # Title for the Shiny Application
+               titlePanel('Course Data'),
+               
+               mainPanel(
+                 # Add a textInput that allows the user to enter the class for which the grade data is needed.
+                 selectInput('course.var', label = 'Select Course', choices = unique.course),
+                 
+                 uiOutput('depControl'),
+                 
+                 # Displays the scatter plot.
+                 plotlyOutput('plotly')
+               )
+             )),
+             tabPanel("Professor Search", fluidPage(
+               "Select a professor to see grading statistics.",
+               selectInput('prof.var', label = 'Select Professor', choices = unique.prof),
+               plotlyOutput('prof_graph'),
+               dataTableOutput('prof_table')
+               
+             )),
+             tabPanel("Department Extremes", fluidPage(
+               titlePanel("View the Easiest and Hardest Professors in each Department"),
+               selectInput('dept.var', label = 'Select Department', choices = unique.dept),
+               plotlyOutput('extremes')
+             ))
+             
+  )
+)
